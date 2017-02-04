4fda243ebce12704de28e7d19fff447c7af86381
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-02-04 16:43:29 +0000

    .

--- src/emu3d.cpp
+++ src/emu3d.cpp
@@ -1,5 +1,5 @@
 # include "emu3d.hpp"
 
-mdl::uint_t mdl::emu3d(uint_t __xa, uint_t __ya, uint_t __za, uint_t __xa_len, uint_t __ya_len, uint_t __za_len) {
-    return (__xa + (__ya * __xa_len) + (__za * (__xa_len * __ya_len)));
+mdl::uint_t mdl::emu3d(uint_t __xa, uint_t __ya, uint_t __za, uint_t __xa_len, uint_t __ya_len, uint_t __za_len, uint_t __offset) {
+    return ((__xa + (__ya * __xa_len) + (__za * (__xa_len * __ya_len))) * __offset);
 }
