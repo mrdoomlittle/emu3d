c0c31bfd87b3bfb6a8fb8f5091d14e7a930e25d4
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-05-07 11:46:16 +0100

    .

--- src/emu3d.cpp
+++ src/emu3d.cpp
@@ -1,5 +1,4 @@
 # include "emu3d.hpp"
-
-mdl::uint_t mdl::emu3d(uint_t __xa, uint_t __ya, uint_t __za, uint_t __xa_len, uint_t __ya_len, uint_t __za_len, uint_t __offset) {
-    return ((__xa + (__ya * __xa_len) + (__za * (__xa_len * __ya_len))) * __offset);
+mdl::uint_t mdl::emu3d(uint_t __xa, uint_t __ya, uint_t __za, uint_t __xa_len, uint_t __ya_len, uint_t __za_len, uint_t __fs) {
+    return ((__xa + (__ya * __xa_len) + (__za * (__xa_len * __ya_len))) * __fs);
 }
--- src/emu3d.hpp
+++ src/emu3d.hpp
@@ -2,7 +2,7 @@
 # define __emu3d__hpp
 # include <eint_t.hpp>
 namespace mdl {
-    uint_t emu3d(uint_t __xa, uint_t __ya, uint_t __za, uint_t __xa_len, uint_t __ya_len, uint_t __za_len, uint_t __offset);
+    uint_t emu3d(uint_t __xa, uint_t __ya, uint_t __za, uint_t __xa_len, uint_t __ya_len, uint_t __za_len, uint_t __fs);
 }
 
 # endif /*__emu3d__hpp*/
