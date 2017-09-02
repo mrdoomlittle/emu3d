7d9e538508236e4731f29e71080dc78b5a301a07
MrDoomLittle <x-master-of-disaster-x@hotmail.com>
2017-09-02 15:40:47 +0100

    .

--- src/emu3d.hpp
+++ src/emu3d.hpp
@@ -1,6 +1,6 @@
 # ifndef __emu3d__hpp
 # define __emu3d__hpp
-# include <eint_t.hpp>
+# include <mdlint.h>
 namespace mdl {
     uint_t emu3d(uint_t __xa, uint_t __ya, uint_t __za, uint_t __xa_len, uint_t __ya_len, uint_t __za_len, uint_t __fs);
 }
